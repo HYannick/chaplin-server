eecbca6dbb13e250d946ebba29aa3eebb9eb64dc
yhoussin <yhoussin@sqli.com>
2017-09-07 12:11:13 +0200

    refacto

--- server/controllers/email_controller.js
+++ server/controllers/email_controller.js
@@ -10,6 +10,26 @@ const transporter = nodemailer.createTransport({
     auth: authConfig,
     tls: { rejectUnauthorized: false }
 });
+const generateMailTpl = (email, password, link) => {
+    return `
+    <div class="email__wrapper" style="color: #333; max-width: 500px; width: 100%; margin: 0 auto; border: 1px solid #000; padding: 20px;">
+      <img alt="chaplin_logo" style="text-align: center;display: block;margin: 15px auto 30px;"src="http://ayho.fr/uploads/mini_logo.png"/>
+      <h2 style="color: #333;text-align: center; display: block; padding: 20px;">Bienvenue parmi nous !</h2>
+      <p style="color: #333;">Le cinéma Charlie Chaplin à Montmélian est heureux de vous compter parmi ses membres.
+      <br><br>Afin de vous inscrire à des permanences et d'échanger avec nous sur notre forum, vous aurez besoin des identifiants suivants :</p>
+      <br>
+      <div style="display:table; margin: 0 auto;">
+      <p><span style="display:inline-block; width: 100px; font-weight:bold;">Email</span> : ${email}</p>
+      <p><span style="display:inline-block; width: 100px; font-weight:bold;">Mot de passe</span> : ${password}</p>
+      </div>
+      <br>
+      <p> Pour valider votre inscription, veuillez confirmer votre adresse email en cliquant sur ce lien : <a style="color: #333; font-weight: bold" href="${link}" target="_blank">${link}</a></p>
+      <br>
+      <p style="color: #333;" >A très bientôt,</p>
+      <p style="color: #333;">L'équipe du cinéma Charlie Chaplin</p>
+    </div>
+    `
+}
 let rand, host, link, mailOptions;
 
 module.exports = {
@@ -49,32 +69,12 @@ module.exports = {
         const { email, password } = req.body;
         rand = Math.floor((Math.random() * 100) + 54);
         host = req.get('host');
-        console.log(host);
         link = `http://${req.get('host')}/api/sendmail/verify?id=${rand}`;
-
         mailOptions = {
             from: authConfig.user,
             to: email,
             subject: `Vos informations`,
-            html: `
-            <div class="email__wrapper" style="color: #333; max-width: 500px; width: 100%; margin: 0 auto; border: 1px solid #000; padding: 20px;">
-              <img alt="chaplin_logo" style="text-align: center;display: block;margin: 15px auto 30px;"src="http://ayho.fr/uploads/mini_logo.png"/>
-              <h2 style="color: #333;text-align: center; display: block; padding: 20px;">Bienvenue parmi nous !</h2>
-              <p style="color: #333;">Le cinéma Charlie Chaplin à Montmélian est heureux de vous compter parmi ses membres.
-              <br><br>Afin de vous inscrire à des permanences et d'échanger avec nous sur notre forum, vous aurez besoin des identifiants suivants :</p>
-              <br>
-              <div style="display:table; margin: 0 auto;">
-              <p><span style="display:inline-block; width: 100px; font-weight:bold;">Email</span> : ${email}</p>
-              <p><span style="display:inline-block; width: 100px; font-weight:bold;">Mot de passe</span> : ${password}</p>
-              </div>
-              
-              <br>
-              <p> Pour valider votre inscription, veuillez confirmer votre adresse email en cliquant sur ce lien : <a style="color: #333; font-weight: bold" href="${link}" target="_blank">${link}</a></p>
-              <br>
-              <p style="color: #333;" >A très bientôt,</p>
-              <p style="color: #333;">L'équipe du cinéma Charlie Chaplin</p>
-            </div>
-            `
+            html: generateMailTpl(email, password, link)
         }
         transporter.sendMail(mailOptions, function(error, info) {
             if (error) {
