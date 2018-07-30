b8c1b19f5c95c442fd287a476bc571d18ff1e31d
ayho <ayho.nine@gmail.com>
2018-07-30 02:32:51 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const app = require('./server/app');
 const port = process.env.PORT || 4001;
-const io = require('socket.io')(app.listen(port, () => {
+app.listen(port, () => {
     console.log('Running on port :: ', port);
-}))
-const chat = require('./server/controllers/chat_controller');
-chat.initCon(io)
+})
