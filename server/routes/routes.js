b44ff27ecdee70196df8ad6d8c96ee2f3f319fe0
ayho <ayho.nine@gmail.com>
2018-07-30 02:42:53 +0200

    fix auth

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,6 @@
 //Passport config
 const passport = require('passport');
+const passportService = require('../config/passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
